ce552373fd7d41933857c5840448221f9a746a53
kaybriyel <72874840+kaybriyel@users.noreply.github.com>
2023-01-06 17:13:23 +0700

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '',
+  title: 'MPWT - Docs',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '',
+        title: 'MPWT - Docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
